18ccd66942404f7abfd9aaa8c92b15b9bb3fe13a
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-10-18 18:05:41 +0200

    refactor: extract derive implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    nonstandard_style,
+    future_incompatible,
+    clippy::all,
+    clippy::suspicious,
+    clippy::style,
+    clippy::complexity,
+    clippy::perf,
+    clippy::pedantic
+)]
+#![forbid(unsafe_code)]
+
 pub use tagname_derive::TagName;
 
 pub trait TagName {
--- tagname_derive/src/lib.rs
+++ tagname_derive/src/lib.rs
@@ -1,23 +1,46 @@
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    nonstandard_style,
+    future_incompatible,
+    clippy::all,
+    clippy::suspicious,
+    clippy::style,
+    clippy::complexity,
+    clippy::perf,
+    clippy::pedantic
+)]
+#![allow(clippy::doc_markdown, clippy::missing_panics_doc)]
+#![forbid(unsafe_code)]
+
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::punctuated::Pair;
 use syn::{Ident, Variant};
 
+struct TaggedUnion {
+    name: Ident,
+    variants_with_fields: Vec<Ident>,
+    variants_without_fields: Vec<Ident>,
+}
+
+/// Generates derive implementation for TagName
 #[proc_macro_derive(TagName)]
 pub fn tagname_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
-    // Build the trait implementation
-    impl_variant_derive_macro(ast)
+    generate_code(traverse_ast(ast))
 }
 
-fn impl_variant_derive_macro(ast: syn::DeriveInput) -> TokenStream {
+fn traverse_ast(ast: syn::DeriveInput) -> TaggedUnion {
     let name = &ast.ident;
     let variants: Vec<Variant> = match ast.data {
         syn::Data::Enum(enum_data) => enum_data
             .variants
             .into_pairs()
             .into_iter()
-            .map(|pair| pair.into_value())
+            .map(Pair::into_value)
             .collect(),
         _ => panic!("cannot derive TagName for non-enum types"),
     };
@@ -29,16 +52,27 @@ fn impl_variant_derive_macro(ast: syn::DeriveInput) -> TokenStream {
         .into_iter()
         .map(|v| v.ident)
         .collect();
+    TaggedUnion {
+        name: name.clone(),
+        variants_without_fields: no_field_tags,
+        variants_with_fields: with_field_tags,
+    }
+}
+
+fn generate_code(tagged_union: TaggedUnion) -> TokenStream {
+    let name = tagged_union.name;
+    let variants_without_fields = tagged_union.variants_without_fields;
+    let variants_with_fields = tagged_union.variants_with_fields;
 
     let gen = quote! {
         impl TagName for #name {
             fn tag_name(&self) -> &'static str {
                 match self {
                     #(
-                        #name::#with_field_tags(_) => stringify!(#with_field_tags)
+                        #name::#variants_with_fields(_) => stringify!(#variants_with_fields)
                     ),*,
                     #(
-                        #name::#no_field_tags => stringify!(#no_field_tags)
+                        #name::#variants_without_fields => stringify!(#variants_without_fields)
                     ),*
                 }
             }
