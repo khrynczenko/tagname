41b6cdcba47b703fc8e590af24cdf3b5c45bd359
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-10-29 12:23:42 +0200

    refactor: use single vector for all tags
    Instead of using one vector for empty tags and a separate
    vector for not empty tags, which cause a lot of boilerprate and
    repetition, now store all tag data in a single vector.

--- tagname_derive/src/lib.rs
+++ tagname_derive/src/lib.rs
@@ -16,17 +16,26 @@
 
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::__private::TokenStream2;
 use syn::punctuated::Pair;
 use syn::{Ident, Variant};
 
+struct TagData {
+    ident: Ident,
+}
+
+enum Tag {
+    Empty(TagData),
+    NotEmpty(TagData),
+}
+
 struct TaggedUnion {
     name: Ident,
-    variants_with_fields: Vec<Ident>,
-    variants_without_fields: Vec<Ident>,
+    tags: Vec<Tag>,
 }
 
 /// Generates derive implementation for TagName
-#[proc_macro_derive(TagName)]
+#[proc_macro_derive(TagName, attributes(tag))]
 pub fn tagname_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
@@ -49,42 +58,49 @@ fn traverse_ast(ast: syn::DeriveInput) -> TaggedUnion {
         !variants.is_empty(),
         "cannot derive TagName for empty enum types"
     );
-
-    let variants_with_fields = variants.iter().cloned().filter(|v| !v.fields.is_empty());
-    let variants_without_fields = variants.iter().cloned().filter(|v| v.fields.is_empty());
-    let with_field_tags: Vec<Ident> = variants_with_fields.into_iter().map(|v| v.ident).collect();
-    let no_field_tags: Vec<Ident> = variants_without_fields
+    let tags = variants
         .into_iter()
-        .map(|v| v.ident)
+        .map(|v| {
+            if v.fields.is_empty() {
+                Tag::Empty(TagData { ident: v.ident })
+            } else {
+                Tag::NotEmpty(TagData { ident: v.ident })
+            }
+        })
         .collect();
+
     TaggedUnion {
         name: name.clone(),
-        variants_without_fields: no_field_tags,
-        variants_with_fields: with_field_tags,
+        tags,
+    }
+}
+
+fn generate_match_arm(enum_name: Ident, tag: Tag) -> TokenStream2 {
+    match tag {
+        Tag::Empty(data) => {
+            let ident = data.ident;
+            quote!(#enum_name::#ident => stringify!(#ident),)
+        }
+        Tag::NotEmpty(data) => {
+            let ident = data.ident;
+            quote!(#enum_name::#ident(..) => stringify!(#ident),)
+        }
     }
 }
 
 fn generate_code(tagged_union: TaggedUnion) -> TokenStream {
     let name = tagged_union.name;
-    let variants_without_fields = tagged_union.variants_without_fields;
-    let variants_with_fields = tagged_union.variants_with_fields;
-
-    let comma = if variants_with_fields.is_empty() {
-        quote!()
-    } else {
-        quote!(,)
-    };
+    let match_arms: Vec<TokenStream2> = tagged_union
+        .tags
+        .into_iter()
+        .map(|tag| generate_match_arm(name.clone(), tag))
+        .collect();
 
     let gen = quote! {
         impl TagName for #name {
             fn tag_name(&self) -> &'static str {
                 match self {
-                    #(
-                        #name::#variants_with_fields(..) => stringify!(#variants_with_fields)
-                    ),*#comma
-                    #(
-                        #name::#variants_without_fields => stringify!(#variants_without_fields)
-                    ),*
+                    #(#match_arms)*
                 }
             }
         }
